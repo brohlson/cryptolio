70bfc5a6c814e7a2140d9504e7b62e1ff57ffcb1
TJ <cooploops@gmail.com>
2017-12-05 15:38:28 -0800

    made changes to show data without any authentication middleware being used

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,7 +6,8 @@ const router  = express.Router();
 const dashboard_controller = require('../controllers/dashboard_controller');
 const isAuthenticated = require('../config/middleware/isAuthenticated');
 
-router.get('/', isAuthenticated, dashboard_controller.index);
+router.get('/', dashboard_controller.index);
+// router.get('/', isAuthenticated, dashboard_controller.index); will use this when authenication is implemented
 // router.post('/new', dashboard_controller.search);
 
 module.exports = router;
