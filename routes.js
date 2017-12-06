4fdcc717c29a1db52c5d6781aeb81510dd275d41
Chase Ohlson <chaseohlson@gmail.com>
2017-12-05 18:51:03 -0800

    cannot create new view

--- routes.js
+++ routes.js
@@ -5,8 +5,10 @@ module.exports = function(app){
             var portfolio = require('./routes/portfolio');
             var settings = require('./routes/settings');
             var profile = require('./routes/profile');
+            var login = require('./routes/login');
     
             app.use('/', dashboard);
+            app.use('/login', login);
             // app.use('/portfolio', portfolio);
             // app.use('/profile', profile);
             // app.use('/settings', settings);
---
+++ routes/login.js
@@ -0,0 +1,13 @@
+// dashboard will only show current coins owned
+
+const express = require('express');
+const router  = express.Router();
+
+const login_controller = require('../controllers/login_controller');
+const isAuthenticated = require('../config/middleware/isAuthenticated');
+
+router.get('/login', login_controller.index);
+// router.get('/', isAuthenticated, dashboard_controller.index); will use this when authenication is implemented
+// router.post('/new', dashboard_controller.search);
+
+module.exports = router;
