92b7578359a050e4d8eb4be6661260300526b77c
TJ <cooploops@gmail.com>
2017-12-06 11:14:12 -0800

    fix routes/login.js path to work correctly

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ const router  = express.Router();
 const login_controller = require('../controllers/login_controller');
 const isAuthenticated = require('../config/middleware/isAuthenticated');
 
-router.get('/login', login_controller.index);
+router.get('/', login_controller.index);
 // router.get('/', isAuthenticated, dashboard_controller.index); will use this when authenication is implemented
 // router.post('/new', dashboard_controller.search);
 
