84248b846c99efd3b6b4c7d8b1a88a9bd15c0cc6
TJ <cooploops@gmail.com>
2017-12-10 23:02:45 -0800

    registering a new user works now

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,8 @@ console.log("It works");
 
 $(document).ready(function(){
 	$('#nav-icon1').click(function(){
-	$(this).toggleClass('open');
-	$('#menu').toggleClass('show');
+		$(this).toggleClass('open');
+		$('#menu').toggleClass('show');
 	});
 
 	    // Modal init 
@@ -16,6 +16,30 @@ $(document).ready(function(){
 		$('#login-form').toggleClass('show');
 		$('#sign-up-form').toggleClass('remove');
 		$('#returning').toggleClass('remove');
-		});	  
+	});
+
+			// signUp
+
+	$("#sign-up").on("click", function(event){
+		var signUpEmail = $("#signUpEmail").val().trim();
+		var signUpPW = $("#signUpPass").val().trim();
+		event.preventDefault();
+		console.log("hi");
+		console.log(signUpEmail);
+		console.log(signUpPW);
+		$.post("/login/signup",{
+			email: signUpEmail,
+			password: signUpPW
+		}).then(function(data){
+			if(data.duplicateUser){
+				alert("sorry, that email is already in use");
+			} else {
+				window.location.replace = data.redirect;
+			}
+		}).catch(function(err){
+			console.log(err);
+		})
+	});
+
 });
 
--- routes/login.js
+++ routes/login.js
@@ -6,8 +6,8 @@ const router  = express.Router();
 const login_controller = require('../controllers/login_controller');
 const isAuthenticated = require('../config/middleware/isAuthenticated');
 
-router.get('/', login_controller.index);
+// router.get('/', login_controller.index);
 // router.get('/', isAuthenticated, dashboard_controller.index); will use this when authenication is implemented
-// router.post('/new', dashboard_controller.search);
+router.post('/signup',login_controller.signUpUser);
 
 module.exports = router;
